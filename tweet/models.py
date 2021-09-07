6129f2afed2d9e6862a71e092ce5f73d0b307a81
BethanyFolino <bethany.folino@gmail.com>
2021-09-07 15:03:56 -0700

    Added tweet model

--- tweet/models.py
+++ tweet/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
-# from twitteruser.models import TwitterUser
+from django.contrib.auth.models import AbstractUser
+from twitteruser.models import TwitterUser
 
 # Create your models here.
-# class Tweet(models.Model):
-#     text = models.CharField(max_length=140)
-#     date_created = models.DateTimeField(null=True, blank=True, auto_now_add=True)
-#     created_by = models.ForeignKey(TwitterUser, on_delete=models.CASCADE, null=True, blank=True)
+class Tweet(models.Model):
+    text = models.CharField(max_length=140)
+    date_created = models.DateTimeField(null=True, blank=True, auto_now_add=True)
+    created_by = models.ForeignKey(TwitterUser, on_delete=models.CASCADE, null=True, blank=True)
 
-#     def __str__(self):
-#         return self.text
+    def __str__(self):
+        return self.text
